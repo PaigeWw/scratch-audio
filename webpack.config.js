45306fadda9bde659683d27e3889164efd01cc68
yunpeiWang <yunpei.wang@miaocode.com>
2017-08-25 11:36:48 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+    devtool: 'cheap-module-source-map',
+    entry: {
+        dist: './src/index.js'
+    },
+    output: {
+        path: __dirname,
+        library: 'AudioEngine',
+        libraryTarget: 'commonjs2',
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            include: path.resolve(__dirname, 'src'),
+            query: {
+                presets: ['es2015']
+            }
+        }]
+    }
+};
